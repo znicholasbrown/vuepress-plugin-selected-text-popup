46e210566303cfa7352972efdc6d2efd0769d627
nicholas <znicholasbrown@gmail.com>
2019-12-04 20:43:47 -0500

    Add client root mixin

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const { path } = require("@vuepress/shared-utils");
 module.exports = (options = {}, ctx) => ({
   define: {},
   globalUIComponents: ["selectTextPopup"],
-  enhanceAppFiles: [path.resolve(__dirname, "appLevelEnhancement.js")]
+  enhanceAppFiles: [path.resolve(__dirname, "appLevelEnhancement.js")],
+  clientRootMixin: path.resolve(__dirname, "clientRootMixin.js")
 });
